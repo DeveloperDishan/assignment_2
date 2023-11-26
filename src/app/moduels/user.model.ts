93aa7bb03b6e8887dcb89a4931c9f15da75dc61a
DeveloperDishan <dishanr2000@gmail.com>
2023-11-27 01:31:33 +0600

    password hash

--- src/app/moduels/user.model.ts
+++ src/app/moduels/user.model.ts
@@ -1,7 +1,7 @@
 import { Schema, model } from 'mongoose';
 import { TUser, TOrders } from './user/user.interface';
-// import bcrypt from 'bcrypt';
-// import config from '../config';
+import bcrypt from 'bcrypt';
+import config from '../config';
 
 const orderSchema = new Schema<TOrders>({
   productName: String,
@@ -58,6 +58,21 @@ const userSchema = new Schema<TUser>({
   orders: {
     type: [orderSchema],
   },
+});
+
+// pre save middlewere hook : will work on create() save()
+
+userSchema.pre('save', async function (next) {
+  // console.log(this, 'pre hook: we will save data');
+
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  // hashing password and save into db
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
 });
 
 export const UserModel = model<TUser>('User', userSchema);
